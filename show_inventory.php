e49a62aee05f0ba3a12fc21676f2889c820c7f6f
rn12079 <rn12079@hotmail.com>
2017-10-29 17:03:53 +0100

    fixed > on tr line

--- show_inventory.php
+++ show_inventory.php
@@ -551,8 +551,8 @@ function toggle_dtrange(){
                   
 
                   if ($logged) 
-                  echo "ondblclick=\"window.location.href='".$url.$row["id"]."'\">";
-                  echo"<td>".$row["date"]."</td><td>".$row["receiver"]."</td><td>".$row["product_name"]."</td><td>".$row["supplier"]."</td><td>".$row["product_type"];
+                  echo "ondblclick=\"window.location.href='".$url.$row["id"]."'\"";
+                  echo"><td>".$row["date"]."</td><td>".$row["receiver"]."</td><td>".$row["product_name"]."</td><td>".$row["supplier"]."</td><td>".$row["product_type"];
                   echo "</td><td>".$row["product_sub_type"]."</td><td class='number'>".number_format($row["cases"],2)."</td><td class='number'>";
 
                   // Unit price calculation + arrow sign upon price increase of decrease
